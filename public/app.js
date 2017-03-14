2f58fdc0eb878249b0538ac76b13dc2b9c47678f
Marius Braathen <marius.brathen@me.com>
2017-03-14 10:52:20 +0100

    Test av auth ved hjelp av Google

--- public/app.js
+++ public/app.js
@@ -1,4 +1,48 @@
-(function() {
+function(){
+    const config = {
+            apiKey: "AIzaSyA6ll2lfKu0gxOcnlfqb54IRNLaRUUil8Q",
+            authDomain: "randtest-e8199.firebaseapp.com",
+            databaseURL: "https://randtest-e8199.firebaseio.com",
+            storageBucket: "randtest-e8199.appspot.com",
+            messagingSenderId: "109752125195"
+        };
+        firebase.initializeApp(config);
+
+
+var provider = new firebase.auth.GoogleAuthProvider(); 
+
+    function googleSignIn(){
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user.displayName);
+        }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*(function() {
         const config = {
             apiKey: "AIzaSyA6ll2lfKu0gxOcnlfqb54IRNLaRUUil8Q",
             authDomain: "randtest-e8199.firebaseapp.com",
@@ -52,4 +96,6 @@
 
         	}
         });
-}());
+}()); 
+*/ 
+
