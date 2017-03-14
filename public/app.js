43d39b421fa3e85f27461d4a74b05b5e04841da7
Marius Braathen <marius.brathen@me.com>
2017-03-14 14:23:46 +0100

    Admin login, 'ferdig'

--- public/app.js
+++ public/app.js
@@ -36,7 +36,7 @@ var provider = new firebase.auth.GoogleAuthProvider();
         const txtEmail = document.getElementById('txtEmail');
         const txtPassword = document.getElementById('txtPassword');
         const btnLogin = document.getElementById('btnLogin');
-        const btnSignUp = document.getElementById('btnSignUp');
+        //const btnSignUp = document.getElementById('btnSignUp');
         const btnLogout = document.getElementById('btnLogout'); 
 
         btnLogin.addEventListener('click', e => {
@@ -54,22 +54,22 @@ var provider = new firebase.auth.GoogleAuthProvider();
         });
 
         // add signup event
-        btnSignUp.addEventListener('click', e => {
+        //btnSignUp.addEventListener('click', e => {
             // Get email and password
-            const email = txtEmail.value;
-            const pass = txtPassword.value;
-            const auth = firebase.auth();
+            //const email = txtEmail.value;
+            //const pass = txtPassword.value;
+            //const auth = firebase.auth();
             //sign in
-            const promise = auth.createUserWithEmailAndPassword(email, pass);
-            promise.catch(e => console.log(e.message));
-        });
+            //const promise = auth.createUserWithEmailAndPassword(email, pass);
+            //promise.catch(e => console.log(e.message));
+        //});
 
         //add realtime listener
         firebase.auth().onAuthStateChanged(firebaseUser => {
             if(firebaseUser) {
                 console.log(firebaseUser);
                 btnLogout.classList.remove('hide');
-            } else {
+            } else { 
                 console.log('not logged in');
                 btnLogout.classList.add('hide');
 
