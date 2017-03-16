7645689b0191424950c811e3aea5c94256cd7ff8
Marius Braathen <marius.brathen@me.com>
2017-03-16 11:39:33 +0100

    fix

--- public/admin.js
+++ public/admin.js
@@ -8,42 +8,21 @@
         };
         firebase.initializeApp(config);
 
-
-/* Alternativ inloggingen ved hjelp av Google Auth
-var provider = new firebase.auth.GoogleAuthProvider(); 
-    function googleAuth(){
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user.displayName);
-        }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        });
-    }
-*/
-
 (function() {
 
         // get elements
         const txtEmail = document.getElementById('txtEmail');
         const txtPassword = document.getElementById('txtPassword');
         const btnLogin = document.getElementById('btnLogin');
-        //const btnSignUp = document.getElementById('btnSignUp');
         const btnLogout = document.getElementById('btnLogout'); 
 
         btnLogin.addEventListener('click', e => {
+            
             // Get email and password
             const email = txtEmail.value;
             const pass = txtPassword.value;
             const auth = firebase.auth();
+            
             //sign in
             const promise = auth.signInWithEmailAndPassword(email, pass);
             promise.catch(e => console.log(e.message));
@@ -52,17 +31,6 @@ var provider = new firebase.auth.GoogleAuthProvider();
         btnLogout.addEventListener('click', e => {
             firebase.auth().signOut();
         });
-
-        // add signup event
-        //btnSignUp.addEventListener('click', e => {
-            // Get email and password
-            //const email = txtEmail.value;
-            //const pass = txtPassword.value;
-            //const auth = firebase.auth();
-            //sign in
-            //const promise = auth.createUserWithEmailAndPassword(email, pass);
-            //promise.catch(e => console.log(e.message));
-        //});
 
         //add realtime listener
         firebase.auth().onAuthStateChanged(firebaseUser => {
